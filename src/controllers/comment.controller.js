2f0f4dc4ba33a4099dca6c99a69e7964fc74f671
Naomi <naomistrall@hotmail.com>
2023-08-24 00:47:06 -0300

    Alteração para receber se o user_id segue quem comentou

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,31 +1,31 @@
-
-import { getCommentsByPost, insertComments } from "../repositories/comment.reository.js";
+import {
+  getCommentsByPost,
+  insertComments,
+} from "../repositories/comment.repository.js";
 
 export async function postComments(req, res) {
+  const { user_id } = res.locals;
+  const { comment, postId } = req.body;
 
-    const { user_id } = res.locals;
-    const { comment, postId } = req.body;
-
-    try {
-        await insertComments(comment, postId, user_id)
-        res.sendStatus(201);
-    } catch (err) {
-        console.log(err);
-        res.status(500).send(err.message);
-    }
+  try {
+    await insertComments(comment, postId, user_id);
+    res.sendStatus(201);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err.message);
+  }
 }
 
 export async function getComments(req, res) {
+  const { user_id } = res.locals;
+  const { postId } = req.params;
 
-    const { id } = req.params;
-        console.log(id);
-    try {
-        const commentList = await getCommentsByPost(id)
+  try {
+    const commentList = await getCommentsByPost(postId, user_id);
 
-        res.status(200).send(commentList.rows);
-    } catch (err) {
-        console.log(err);
-        res.status(500).send(err.message);
-    }
+    res.status(200).send(commentList.rows);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err.message);
+  }
 }
-
--- src/routes/comments.routes.js
+++ src/routes/comments.routes.js
@@ -1,13 +1,20 @@
-import { Router } from 'express';
-import validateSchema from '../middlewares/validateSchema.js';
-import { authorizationValidate } from '../middlewares/authorization.js';
-import { commentSchema } from '../schemas/comment.schema.js';
-import { getComments, postComments } from '../controllers/comment.controller.js';
-
+import { Router } from "express";
+import validateSchema from "../middlewares/validateSchema.js";
+import { authorizationValidate } from "../middlewares/authorization.js";
+import { commentSchema } from "../schemas/comment.schema.js";
+import {
+  getComments,
+  postComments,
+} from "../controllers/comment.controller.js";
 
 const routerComment = Router();
 
-routerComment.post("/comments", authorizationValidate, validateSchema(commentSchema), postComments);
-routerComment.get("/comments/:id", getComments);
+routerComment.post(
+  "/comments",
+  authorizationValidate,
+  validateSchema(commentSchema),
+  postComments
+);
+routerComment.get("/comments/:postId", authorizationValidate, getComments);
 
 export default routerComment;
